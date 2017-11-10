bd174b607520d54cb41f8101e4eccedd8f34d47f
Joshua Schers <joshua10@hetnet.nl>
2017-11-10 11:16:37 +0100

    added first working trafficsource

--- Hub/Hub.go
+++ Hub/Hub.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"time"
 
-	calendar "github.com/joshua22s/Personal-Assistant/CalendarSource"
+	calendar "github.com/joshua22s/Personal-Assistant/calendarsource"
+	traffic "github.com/joshua22s/Personal-Assistant/trafficsource"
 )
 
 func main() {
 	calendar.Start()
-	fmt.Println(calendar.GetAppointments(time.Now(), time.Now().Add(time.Hour*24)))
+	appointments := calendar.GetAppointments(time.Now(), time.Now().Add(time.Hour*24))
+	fmt.Println(appointments)
+	traffic.Start()
+	fmt.Println(traffic.GetTravelTime("Hoogstraat 39, Beringe", appointments[0].Location, "driving", appointments[0].StartTime))
 }
