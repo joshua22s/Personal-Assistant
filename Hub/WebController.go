0f191c800d267ade25eb4d006db71295345c2c0e
Joshua Schers <joshua10@hetnet.nl>
2017-11-10 14:43:26 +0100

    added basic webpages with templates

--- Hub/WebController.go
+++ Hub/WebController.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"html/template"
-	"log"
+	"io/ioutil"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-func startWebServer() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./web/")))
-	log.Fatal(http.ListenAndServe(":8000", r))
+var tPath = "./temps/"
+var dPath = "./data/"
+
+var templates = template.Must(template.ParseFiles(tPath+"home.html", dPath+"header.html", dPath+"head.html", dPath+"footer.html"))
+
+//var templateDirs = []string{"temps", "data"}
+
+func rootHandler(wr http.ResponseWriter, req *http.Request) {
+	title := "hello"
+
+	headFile, headErr := ioutil.ReadFile(dPath + "head.html")
+	headerFile, headerErr := ioutil.ReadFile(dPath + "header.html")
+	footerFile, footErr := ioutil.ReadFile(dPath + "footer.html")
+
+	if headErr != nil || headerErr != nil || footErr != nil {
+		http.Error(wr, headErr.Error(), http.StatusInternalServerError)
+		http.Error(wr, headerErr.Error(), http.StatusInternalServerError)
+		http.Error(wr, footErr.Error(), http.StatusInternalServerError)
+	}
+
+	data := map[string]interface{}{
+		"title":  title,
+		"header": string(headerFile),
+		"head":   string(headFile),
+		"footer": string(footerFile),
+	}
+
+	err := templates.ExecuteTemplate(wr, "homeHTML", data)
+
+	if err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-//url: /
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		t, err := template.ParseFiles("web/html/index.html")
-		if err != nil {
-			log.Println(err)
-		}
-		t.Execute(w, nil)
-	}
+func startWebServer() {
+	http.HandleFunc("/", rootHandler)
+	http.ListenAndServe(":8080", nil)
 }
