9915f199804e12110170ffd98351c9248b993e90
Joshua Schers <joshua10@hetnet.nl>
2017-11-10 18:53:39 +0100

    added html for loginpage

--- Hub/WebController.go
+++ Hub/WebController.go
@@ -9,7 +9,7 @@ import (
 var tPath = "./temps/"
 var dPath = "./data/"
 
-var templates = template.Must(template.ParseFiles(tPath+"home.html", dPath+"header.html", dPath+"head.html", dPath+"footer.html"))
+var templates = template.Must(template.ParseFiles(tPath+"home.html", tPath+"login.html", dPath+"header.html", dPath+"loginheader.html", dPath+"head.html", dPath+"footer.html"))
 
 //var templateDirs = []string{"temps", "data"}
 
@@ -40,7 +40,41 @@ func rootHandler(wr http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func loginHandler(wr http.ResponseWriter, req *http.Request) {
+	title := "Login"
+
+	headFile, headErr := ioutil.ReadFile(dPath + "head.html")
+	headerFile, headerErr := ioutil.ReadFile(dPath + "loginheader.html")
+	footerFile, footErr := ioutil.ReadFile(dPath + "footer.html")
+
+	if headErr != nil || headerErr != nil || footErr != nil {
+		http.Error(wr, headErr.Error(), http.StatusInternalServerError)
+		http.Error(wr, headerErr.Error(), http.StatusInternalServerError)
+		http.Error(wr, footErr.Error(), http.StatusInternalServerError)
+	}
+
+	data := map[string]interface{}{
+		"title":       title,
+		"head":        string(headFile),
+		"loginheader": string(headerFile),
+		"footer":      string(footerFile),
+	}
+
+	err := templates.ExecuteTemplate(wr, "loginHTML", data)
+
+	if err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func startWebServer() {
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//	fs := http.FileServer(http.Dir("data"))
+	//	http.Handle("/css/", fs)
+	//	http.Handle("/js/", fs)
+	//	http.Handle("/fonts/", fs)
+	//	http.Handle("/img/", fs)
 	http.ListenAndServe(":8080", nil)
 }
