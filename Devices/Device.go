0a649f136512c2f271433f0decd5a724c10caea6
Joshua Schers <joshua10@hetnet.nl>
2017-11-11 14:04:06 +0100

    added structs for devices

---
+++ Devices/Device.go
@@ -0,0 +1,5 @@
+package devices
+
+type Device struct {
+	name string
+}
---
+++ Hub/WakeupTimeCalculator.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	calendar "github.com/joshua22s/Personal-Assistant/calendarsource"
+	traffic "github.com/joshua22s/Personal-Assistant/trafficsource"
+)
+
+func calculateWakeUpTime() {
+
+}
