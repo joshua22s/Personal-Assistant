d30a31ed9fda9df9d260efb6d601297d3c34d5b0
Joshua Schers <joshua10@hetnet.nl>
2017-12-26 19:58:16 +0100

    worked on comparing triggers

--- personalassistant/event.go
+++ personalassistant/event.go
@@ -2,20 +2,19 @@ package personalassistant
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/joshua22s/observer"
 )
 
 type Event struct {
 	Name     string
-	triggers map[observer.Trigger]bool
+	triggers map[*observer.Trigger]bool
 	actions  []Action
 }
 
 func NewEvent(name string) *Event {
 	e := Event{Name: name}
-	e.triggers = make(map[observer.Trigger]bool, 0)
+	e.triggers = make(map[*observer.Trigger]bool, 0)
 	e.actions = make([]Action, 0)
 	return &e
 }
@@ -26,15 +25,15 @@ func (this *Event) AddAction(action Action) {
 
 func (this *Event) AddTrigger(trigger observer.Trigger) {
 	trigger.AddListener(this)
-	this.triggers[trigger] = false
+	this.triggers[&trigger] = false
 }
 
 func (this *Event) Announce(announcer observer.Trigger) {
 	fmt.Println(len(this.triggers))
 	for trigger, _ := range this.triggers {
 		fmt.Println(trigger)
-		fmt.Println(announcer)
-		if reflect.DeepEqual(trigger, announcer) {
+		fmt.Println(&announcer)
+		if trigger == &announcer {
 			fmt.Println("announce")
 			this.triggers[trigger] = true
 			return
